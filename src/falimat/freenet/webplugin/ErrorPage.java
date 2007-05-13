03a0a12375a919367576a213ca9e2eab31df1fd6
Juiceman <juiceman@xqbd.com>
2007-05-13 20:41:34 +0000

    Trivial.  freenet -> Freenet
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/bookmarkplugin@13237 67a373e5-eb02-0410-a15c-ee090a768436

--- src/falimat/freenet/webplugin/ErrorPage.java
+++ src/falimat/freenet/webplugin/ErrorPage.java
@@ -5,7 +5,7 @@ import falimat.freenet.webplugin.components.MessageArea;
 public class ErrorPage extends HtmlPage {
     public ErrorPage(String msg, Throwable t) {
         super("Error Page"); // TODO: This is weird
-        super.setTitle("Error in freenet plugin: " + msg);
+        super.setTitle("Error in Freenet plugin: " + msg);
         MessageArea messageArea = new MessageArea();
         messageArea.setHeadline(msg);
         messageArea.setMessage("<pre>" + HtmlPage.getStacktrace(t) + "</pre>");
--- src/falimat/freenet/webplugin/KeyNotFoundException.java
+++ src/falimat/freenet/webplugin/KeyNotFoundException.java
@@ -7,6 +7,6 @@ import xomat.util.ParamException;
 
 public class KeyNotFoundException extends ParamException {
     public KeyNotFoundException(String uri) {
-        super("Failed to fetch freenet key  {0}", uri);
+        super("Failed to fetch Freenet key  {0}", uri);
     }
 }
